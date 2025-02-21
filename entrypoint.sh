8a0142da4d4e3a868ff3117965610cacb0b99416
kvv1618 <varun.kesharaju@gmail.com>
2025-02-21 09:37:31 -0500

    correct entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 set -e
-export PYTHONPATH=.
-python3 ./app/kafka/consumer.py &
-uvicorn application:app --host 0.0.0.0 --port 8000 --workers 4
-
-
-#python3 application.py
+uvicorn app:app --host 0.0.0.0 --port 8000 --workers 4
