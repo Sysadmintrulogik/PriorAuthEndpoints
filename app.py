419141d9a7a4088f254b9447982219c2b2ed17a1
Prashanthtrulogik <prasanth.kumar@trulogik.com>
2025-02-25 11:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from .app_create_json import edi_json_bp
 from .app_validate import validate_bp
 from .api_script import edi_bp
 from .priorauth_workflow_2 import wfo_bp
+from .priorauth_workflow_new_post import wfo_new_bp
 
 app = Flask(__name__)
 
@@ -23,6 +24,7 @@ api.register_blueprint(edi_json_bp)
 api.register_blueprint(validate_bp)
 api.register_blueprint(edi_bp)
 api.register_blueprint(wfo_bp)
+api.register_blueprint(wfo_new_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv("APP_PORT", 8000)), debug=bool(os.getenv("DEBUG", False)))
