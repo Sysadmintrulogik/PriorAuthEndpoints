2b8dd66163acd85e2f8ee5caae4c63c1f92be63c
indraniltrulogik <indranil.basu@trulogik.com>
2025-03-03 16:46:29 +0530

    Update create_edi_response.py

--- create_edi_response.py
+++ create_edi_response.py
@@ -13,14 +13,25 @@ claim_values = load_config("custom_edi.config")
 
 @edi_resp_bp.route('/response', methods=['GET', 'POST'])
 def create_edi():
+    claim_values = load_config("custom_edi.config")
+
+    edi_content = ""
     if request.method == "POST":
         try:
             obj = request.get_json()
+            edi_content = generate_edi_278_new(obj)
         except Exception as e:
             return jsonify({"error":  str(e)}), 400
     else:
         blob_url = request.args.get("blob_url")
+        print("Blob URL from GET = ", blob_url)
         if not blob_url:
             return jsonify({"error": "blob_url is required"}), 400
-    
-    return jsonify({"message": "Success"}), 200
+        
+    print("Generated EDI 278 File:")
+    print(edi_content)
+    response = {
+        "message": "DI Response Created based on given Member, Provider, ICD, CPT and Authorization",
+        "data": edi_content
+    }
+    return jsonify(response)
