f9f68da554f43a670c3b35fbcf78d0a039808f6a
indraniltrulogik <indranil.basu@trulogik.com>
2025-03-10 11:26:21 +0530

    Update create_edi.py

--- create_edi.py
+++ create_edi.py
@@ -135,3 +135,29 @@ def create_edi():
         "data": edi_content
     }
     return jsonify(response)
+
+@edi_bp.route('/create_edi_278_new', methods=['GET', 'POST'])
+def create_edi():
+    claim_values = load_config("custom_edi.config")
+
+    edi_content = ""
+    if request.method == "POST":
+        try:
+            obj = request.get_json()
+            edi_content = generate_edi_278_new(obj)
+        except Exception as e:
+            return jsonify({"error":  str(e)}), 400
+    else:
+        blob_url = request.args.get("blob_url")
+        print("Blob URL from GET = ", blob_url)
+        if not blob_url:
+            return jsonify({"error": "blob_url is required"}), 400
+        
+    print("Generated EDI 278 File:")
+    print(edi_content)
+    response = {
+        "message": "EDI 278 Created based on given Member, Provider, ICD, CPT, Submitter Receiver",
+        "data": edi_content
+    }
+    return jsonify(response)
+
